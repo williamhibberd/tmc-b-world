1a3dc56a74c91c2e2f5c4daea23a2d845341e11b
William Hibberd <will@williamhibberd.co.uk>
2022-12-12 15:57:12 +0000

    added info to nav

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -8,11 +8,51 @@ import {
 export default function nav(scene) {
 	return {
 		show: false,
+		showInfo: false,
 		scene: scene,
 		init() {
+			gsap.set("#infoWrapper", { xPercent: 100 });
 			gsap.set("#navWrapper", { xPercent: 100 });
 			gsap.set("#burger-1", { y: -4 });
 			gsap.set("#burger-2", { y: 4 });
+			gsap.set(this.$refs.text, { opacity: 0 });
+			gsap.set(this.$refs.callout, { opacity: 0 });
+		},
+		openInfo() {
+			const openInfo = gsap.timeline({
+				onStart: () => (this.showInfo = true),
+				defaults: { duration: 0.25, ease: "power4.in" },
+			});
+			openInfo.fromTo(
+				"#infoWrapper",
+				{ xPercent: 100 },
+				{ xPercent: 0, duration: 0.5 }
+			);
+			openInfo.fromTo(this.$refs.text, { opacity: 0 }, { opacity: 1 });
+			openInfo.fromTo(
+				this.$refs.callout,
+				{ opacity: 0 },
+				{ opacity: 1 },
+				"<"
+			);
+		},
+		closeInfo() {
+			const closeInfo = gsap.timeline({
+				onComplete: () => (this.showInfo = false),
+				defaults: { duration: 0.25, ease: "power4.in" },
+			});
+			closeInfo.fromTo(
+				"#infoWrapper",
+				{ xPercent: 0 },
+				{ xPercent: 100, duration: 0.5 }
+			);
+			closeInfo.fromTo(this.$refs.text, { opacity: 1 }, { opacity: 0 });
+			closeInfo.fromTo(
+				this.$refs.callout,
+				{ opacity: 1 },
+				{ opacity: 0 },
+				"<"
+			);
 		},
 		openNav() {
 			this.removePageEvents();
@@ -41,7 +81,9 @@ export default function nav(scene) {
 			if (this.show) {
 				this.addPageEvents();
 				const closeNav = gsap.timeline({
-					onComplete: () => (this.show = false),
+					onComplete: () => (
+						(this.show = false), (this.showInfo = false)
+					),
 					defaults: { duration: 0.25, ease: "power4.in" },
 				});
 				closeNav.fromTo(
