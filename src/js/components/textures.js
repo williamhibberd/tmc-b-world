6c600aff0c5ef64b7d42ea428ebc6e7718e30d6a
William Hibberd <will@williamhibberd.co.uk>
2023-01-21 14:49:01 +0000

    update textures

--- src/js/components/textures.js
+++ src/js/components/textures.js
@@ -2,11 +2,15 @@ import * as THREE from "three";
 import { sRGBEncoding } from "three";
 export const textureLoader = new THREE.TextureLoader();
 
-export const lobbyTexture = textureLoader.load("/images/lobby/lobby.jpg");
+export const lobbyTexture = textureLoader.load(
+	"/images/lobby/lobby-hr-20-01-23.jpg"
+);
 lobbyTexture.mapping = THREE.EquirectangularReflectionMapping;
 lobbyTexture.encoding = THREE.sRGBEncoding;
 
-export const libraryTexture = textureLoader.load("/images/library/library.jpg");
+export const libraryTexture = textureLoader.load(
+	"/images/library/library-hr-20-01-23.jpg"
+);
 libraryTexture.mapping = THREE.EquirectangularReflectionMapping;
 libraryTexture.encoding = THREE.sRGBEncoding;
 
