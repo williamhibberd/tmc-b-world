cf3f163f8500b19c706b1616d3a3c1bcc53adb23
William Hibberd <will@williamhibberd.co.uk>
2023-02-20 17:17:56 +0000

    new renders

--- src/js/components/textures.js
+++ src/js/components/textures.js
@@ -3,19 +3,19 @@ import { sRGBEncoding } from "three";
 export const textureLoader = new THREE.TextureLoader();
 
 export const lobbyTexture = textureLoader.load(
-	"/images/lobby/lobby-hr-20-01-23.jpg"
+	"/images/lobby/lobby-hr-20-02-23.jpg"
 );
 lobbyTexture.mapping = THREE.EquirectangularReflectionMapping;
 lobbyTexture.encoding = THREE.sRGBEncoding;
 
 export const libraryTexture = textureLoader.load(
-	"/images/library/library-hr-20-01-23.jpg"
+	"/images/library/library-hr-20-02-23.jpg"
 );
 libraryTexture.mapping = THREE.EquirectangularReflectionMapping;
 libraryTexture.encoding = THREE.sRGBEncoding;
 
 export const outsideTexture = textureLoader.load(
-	"/images/outside/outside-hr-20-01-23.jpg"
+	"/images/outside/outside-hr-20-02-23.jpg"
 );
 outsideTexture.mapping = THREE.EquirectangularReflectionMapping;
 outsideTexture.encoding = THREE.sRGBEncoding;
@@ -23,17 +23,17 @@ outsideTexture.encoding = THREE.sRGBEncoding;
 export const asterixTexture = textureLoader.load("/images/asterix.svg");
 asterixTexture.encoding = THREE.sRGBEncoding;
 
-export const textureVid = document.createElement("video");
-textureVid.src = `/videos/new-scene-re.mp4#t=0.001`;
-textureVid.setAttribute("crossOrigin", "anonymous");
-textureVid.setAttribute("type", "video/mp4");
-textureVid.setAttribute("webkit-playsinline", "");
-textureVid.setAttribute("playsinline", "");
-textureVid.setAttribute("muted", "");
-textureVid.setAttribute("autoplay", "");
-textureVid.setAttribute("loop", "");
+// export const textureVid = document.createElement("video");
+// textureVid.src = `/videos/new-scene-re.mp4#t=0.001`;
+// textureVid.setAttribute("crossOrigin", "anonymous");
+// textureVid.setAttribute("type", "video/mp4");
+// textureVid.setAttribute("webkit-playsinline", "");
+// textureVid.setAttribute("playsinline", "");
+// textureVid.setAttribute("muted", "");
+// textureVid.setAttribute("autoplay", "");
+// textureVid.setAttribute("loop", "");
 // document.body.appendChild(textureVid); //** test video
 
-export const videoTexture = new THREE.VideoTexture(textureVid);
-videoTexture.generateMipmaps = false;
-videoTexture.encoding = sRGBEncoding;
+// export const videoTexture = new THREE.VideoTexture(textureVid);
+// videoTexture.generateMipmaps = false;
+// videoTexture.encoding = sRGBEncoding;
